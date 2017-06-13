378ba8f9fa0a9a34c5429ddfed429cf60e7f3cfb
YecheolKim <yckim@spo.hanyang.ac.kr>
2017-06-13 17:27:39 +0900

    Update 2a.m

--- homework3/2a.m
+++ homework3/2a.m
@@ -1,55 +1,57 @@
-A = [0 3/4 1/4 1;
-     2/3 1 1 1/3;
-     1/4 1/4 1/2 1;
+clear;
+
+A = [0 3/4 1/4 0;
+     2/3 0 0 1/3;
+     1/4 1/4 1/2 0;
      0 0 3/5 2/5];
-A = cumsum(A);
-Zt = [-2;1;1;2];
+Zt = [-2;-1;1;2];
+err = zeros(1,31);
+cnt = 1;
 
 
-for SIG = -10:0.5:40
-    sigma = 10^(SIG/-20);
+for SIG = -40:2:20
+    disp(SIG)
+    sigma = sqrt(10^(SIG/-10));
     for step = 1:1000
         % initialize
-        alpha = zeros(1,100);
-        beta = zeros(1,100);
-        gamma = zeros(1,100);
+        alpha = zeros(4,100);
+        beta = zeros(4,100);
         B = zeros(4,100);
         Z = zeros(1,100);
-        Z(1) = randi(4);
+        Z(1, 1) = randi(4);
         for i = 2:100
-            Z(i) = 1 + sum(rand() > A(Z(i-1),:));
+            Z(1, i) = find(rand<cumsum(A(Z(1,i-1),:)),1);
         end
 
         % calculate B matrix
-        Y = Z + normrnd(0,sigma^2,[1 100]);
-        for i = 1:4
-            B(i,:) = normpdf(Y,Zt(i),sigma^2);
+        Y = Z + normrnd(0,sigma,[1 100]);
+        for i = 1:100
+            B(:,i) = normpdf(Y(i),Zt',sigma);
         end
         
-        % evaluate alpha & betta & gamma
-        alpha(1) = 0.25*B(Z(1),1);
+        % evaluate alpha & beta & gamma by forward, backward recursion
+        alpha(:,1) = 0.25.*B(:,1);
+        beta(:,100) = 1;
+       
         for t = 1:99
-            alpha(t+1) = 0;
-            for i = 1:4
-                alpha(t+1) = alpha(t+1) + alpha(t)*A(Z(t),Z(t+1))*B(Z(t+1),t+1);
-            end 
+            alpha(:,t+1) = alpha(:,t)'*A.*B(:,t+1)';
         end
-        betta(100) = 1;
         for t = 100:-1:2
-            beta(t-1) = 0;
-            for i = 1:4
-                beta(t-1) = beta(t-1) + A(Z(t-1),Z(t))*B(Z(t),t)*beta(t);
-            end
+            beta(:,t-1) = sum(A.*repmat(B(:,t)',4,1).*repmat(beta(:,t)',4,1),2);
         end
-        sum = 0;
-        for t = 1:100
-            sum = sum + alpha(t)*beta(t);
-            gamma(t) = alpha(t)*beta(t);
-        end
-        gamma = gamma./sum;
+        gamma = (alpha.*beta)./repmat(sum(alpha.*beta),4,1);
         
         % MAP estimate
-        [sdf, Zh] = max(gamma);
-        tf = eq(Zh,Z);
+        [~, Zh] = max(gamma);
+        tf = sum(eq(Zh,Z));
+        tf = 100 - tf;
+        
+        err(cnt) = err(cnt) + tf/1000;
     end
+    disp(err(cnt));
+    cnt = cnt+1;
 end
+
+
+figure;
+plot(-40:2:20,err);
